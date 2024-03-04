73b33f0cdee66b22846917194b65ef05cd597ab5
przemek milke <theblimek@gmail.com>
2024-03-04 11:36:56 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SzkołaDotNet
+
+{
+    public class Program
+
+    {
+
+        static void Main(string[] args)
+        {
+            string imie = "Przemek";
+            string nazwisko = "Milke";
+            int wiek = 44;
+            char plec = 'm';
+            string pesel = "79081300000";
+            string numerPracownika = "2509324094";
+
+            Console.WriteLine($"Imię :{imie} , Nazwisko: {nazwisko} , Wiek: {wiek} , Płeć: {plec} " +
+                              $", Pesel: {pesel} ,Numer Pracownika: {numerPracownika}");
+
+            Console.ReadLine();
+
+        }
+    }
+
+}
